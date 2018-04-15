93ac0e86bdd655deceac2d0c5fab9f2d2200bcaf
Timbama <timbama@gmail.com>
2018-04-15 19:54:02 -0400

    fix

--- send_video.py
+++ send_video.py
@@ -1,17 +1,18 @@
 import socket
 import numpy as np
 import cv2
-UDP_IP = '127.0.0.1'                  
-UDP_PORT = 999        
-cap = cv2.VideoCapture('drop.avi')
+UDP_IP = '10.0.1.36'                  
+UDP_PORT = 4096        
+cap = cv2.VideoCapture(0)
 while(True):
    ret, frame = cap.read()
    cv2.imshow('frame',frame)
    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-   d = frame.flatten ()
+   d = frame.flatten() 
    s = d.tostring ()
+   print(len(s))
    for i in range(20):
-        sock.sendto (s[i*184320:(i+1)*184320],(UDP_IP, UDP_PORT))
+        sock.sendto (s[i*46080:(i+1)*46080],(UDP_IP, UDP_PORT))
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
           break
