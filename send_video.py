b5355700f1e527039e7fe6ae0d96d2399b734ec1
Timbama <timbama@gmail.com>
2018-04-16 08:56:20 -0400

    added UDP and TCP sender

--- send_video.py
+++ send_video.py
@@ -2,14 +2,14 @@ import cv2
 import numpy as np
 import socket
 import sys
-import pickle
+import _pickle
 import struct 
 
-cap=cv2.VideoCapture(0)
+cap=cv2.VideoCapture(1)
 clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 clientsocket.connect(('10.0.1.36',8089))
 
 while True:
     ret,frame=cap.read()
-    data = pickle.dumps(frame)
+    data = _pickle.dumps(frame, protocol=2 )
     clientsocket.sendall(struct.pack("L", len(data))+data)
--- sender.py
+++ sender.py
@@ -2,15 +2,13 @@ import socket
 import cv2
 import numpy as np
 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
-IP= "192.168.2.8"
+IP= "10.0.1.36"
 port = 5000
 Message="test".encode()
 cap = cv2.VideoCapture(1)
 sen=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 while True:
         ret, frame = cap.read()
-        result, encimg = cv2.imencode('.jpg', frame)
-        print(encimg.shape)
-        d = encimg.flatten()
-        s = d.tostring()
-        sen.sendto(Message, (IP, port))
+        img_str = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 90])[1].tostring()
+        print(len(img_str))
+        sen.sendto(img_str, (IP, port))
