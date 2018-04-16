15c2252a696b38e472534197862878a8da53fc09
timbama@gmail.com <timbama@gmail.com>
2018-04-16 07:57:04 -0400

    added UDP reciever

---
+++ reciever.py
@@ -0,0 +1,22 @@
+import socket
+import sys
+import cv2
+import _pickle
+import numpy as np
+import struct 
+
+HOST=''
+PORT=5000
+
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s.bind((HOST,PORT))
+print ('Socket created')
+
+t=b''
+while True:
+    data, addr = s.recvfrom(90000)
+    frame = np.fromstring (data, dtype=np.uint8)
+    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+    cv2.imshow("frame",frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
